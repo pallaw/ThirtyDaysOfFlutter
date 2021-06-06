dd7fb55a4536fa47ab0232a58820ae37141b994e
Pallaw Pathak <pallaw.pathak@shipsy.co.in>
2021-06-07 00:44:54 +0530

    Adding Image from asset, google fonts, elevated button, Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:thirty_days_of_flutter/pages/home_page.dart';
 import 'package:thirty_days_of_flutter/pages/login_page.dart';
 
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.light,
-      initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
